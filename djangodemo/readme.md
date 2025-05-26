965e037af2f74241137ac28c9aa1513e8937c509
hedefu <hedefu999@163.com>
2025-05-26 19:34:00 +0800

    0526-O:初识WEB框架django

---
+++ djangodemo/readme.md
@@ -0,0 +1,6 @@
+
+# 第一个django应用
+
+初始化一个django项目 `django-admin startproject ll_project .`
+
+最后的.表示 将 Django 项目的文件生成在当前目录下，直接把管理文件如 manage.py放到当前目录下，避免出现ll_project/ll_project/ 这样的嵌套结构
--- pyspider/tutorial/tutorial/spiders/quotes_spider.py
+++ pyspider/tutorial/tutorial/spiders/quotes_spider.py
@@ -7,14 +7,30 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls=[
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/'
+            'http://quotes.toscrape.com/page/1/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
         
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as file:
-            file.write(response.body)
-        self.log('save file %s' % filename)
+        print(f'西瓜皮：meet {response.url}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('span small::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract()
+            }
+        # 写入response到本地文件
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as file:
+            # file.write(response.body)
+        # self.log('save file %s' % filename)
+
+        # 分页需要跟进链接到下一页,得到这个 /page/2/
+        next_page=response.css('li.next a::attr(href)').extract_first()
+        if(next_page is not None):
+            # 查看response的url：response.url
+            # 相对路径替换到绝对路径的正确位置上可以使用urljoin方法
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page, callback=self.parse)
