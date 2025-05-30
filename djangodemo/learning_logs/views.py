f3dfd9f272dae7d9a97d51465c206bcb7c4ef4d3
hedefu <hedefu999@163.com>
2025-05-30 15:07:02 +0800

    0530-O:entry修改功能

--- djangodemo/learning_logs/views.py
+++ djangodemo/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -52,3 +52,18 @@ def new_entry(request, topic_id):
 
     context = {'topic':topic,'form':form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 通过instance指定库里查出来的原始数据对象，通过data设置表单传入的新值
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
